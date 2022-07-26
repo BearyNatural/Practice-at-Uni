3cfe25f22fec225d3036d0b538392b1a7c20a051
kayhoweaws <kayhowe@amazon.com>
2022-07-26 12:45:44 +1000

    Update Functions, Arrays & files.py

--- Assessment/Functions, Arrays & files.py
+++ Assessment/Functions, Arrays & files.py
@@ -3,7 +3,7 @@
 # Randomised import
 import random
 
-bunnies_got_rabies = []
+bunnies_rolling = []
 
 while True:
     nu_dice_roll = int(input("How many times would you like to roll the dice?\n"))
@@ -11,7 +11,7 @@ while True:
     for roll in range(0, nu_dice_roll):
         dice_roll_choice = random.randint(1,6)
         print(f"You rolled: {dice_roll_choice}")
-        bunnies_got_rabies.append(dice_roll_choice)
+        bunnies_rolling.append(dice_roll_choice)
     new = input("Roll again? y or n\n").lower()
     if new != 'y':
         break
@@ -19,11 +19,11 @@ while True:
 
 def Average(bunnies_got_rabies):
     return sum(bunnies_got_rabies) / len(bunnies_got_rabies)
-average = Average(bunnies_got_rabies)
+average = Average(bunnies_rolling)
 # print(f"Average rolls: {average}")
 
 # sum
-total = sum(bunnies_got_rabies)
+total = sum(bunnies_rolling)
 
 # # average
 # average = total / len(bunnies_got_rabies)
@@ -31,5 +31,5 @@ total = sum(bunnies_got_rabies)
 # final prints
 print(f"Average rolls: {average}")
 print(f"Total of rolls: {total}")
-print(f"Stored dice rolls: {bunnies_got_rabies}")
+print(f"Stored dice rolls: {bunnies_rolling}")
 
