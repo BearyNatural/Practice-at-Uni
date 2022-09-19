33fdafd1d84aeb72bd7e53e553cd5e664fbb8ab8
kayhoweaws <kayhowe@amazon.com>
2022-09-19 15:09:30 +1000

    save

---
+++ Database/20220913_database.sql
@@ -0,0 +1,98 @@
+-- shift+ctrl+e = execute
+select name 
+from sys.databases
+
+create database kayhoweDB
+
+use kayhoweDB;
+create table COMPLAINT(
+    FullName NVARCHAR(100),
+    Mobile INT, --NOT NULL IF NO NULL'S ACCEPTED
+    StaffName NVARCHAR(100),
+    ComplaintDetails NVARCHAR(500),
+    ResolutionDetails NVARCHAR(500),
+    PRIMARY KEY (FullName)
+);
+
+DELETE FROM COMPLAINT;
+
+-- Best Practice
+use kayhoweDB;
+INSERT INTO COMPLAINT(FullName, Mobile, StaffName, ComplaintDetails,ResolutionDetails) VALUES
+('BILL SMITH', 438363012, 'FRED FLINSTONE', 'I AM NOT HAPPY ABOUT SOMETHING', 'WE DID NOTHING'),
+('JANE JONES', 404250670, 'BARNEY RUBBLE', 'I AM NOT HAPPY ABOUT SOMETHING DIFFERENT', 'WE GAVE A REFUND'),
+('JANET JACKSON', NULL, 'FRED FLINSTONE', 'I AM ANGRY ABOUT SOMETHING', 'CLIENT TERMINATED CALL BEFORE RESOLUTION REACHED');
+
+use kayhoweDB;
+INSERT INTO COMPLAINT(FullName, StaffName) VALUES
+('JESSICA SIMPSON', 'FRED FLINSTONE'),
+('MICHAEL HUTCHINSON', 'BARNEY RUBBLE'),
+('KRISTEN DUNNST', 'FRED FLINSTONE');
+
+
+-- Not best practice
+use kayhoweDB;
+INSERT INTO COMPLAINT VALUES
+('BILL SMITH', 438363012, 'FRED FLINSTONE', 'I AM NOT HAPPY ABOUT SOMETHING', 'WE DID NOTHING'),
+('JANE JONES', 404250670, 'BARNEY RUBBLE', 'I AM NOT HAPPY ABOUT SOMETHING DIFFERENT', 'WE GAVE A REFUND'),
+('JANET JACKSON', NULL, 'FRED FLINSTONE', 'I AM ANGRY ABOUT SOMETHING', 'CLIENT TERMINATED CALL BEFORE RESOLUTION REACHED');
+
+
+-- insert a few rows
+
+
+-- select data
+
+select * from complaint;
+
+select *;
+
+-- mandatory keywords - select from
+
+-- select <columns>
+-- from <table> as <alias>
+-- where <rows/BOLEAN CONDITION>;
+
+
+-- PLAY
+
+select c.FullName, c.StaffName
+from complaint as c
+where staffname = 'FRED FLINSTONE';
+
+select *
+from complaint
+where staffname != 'FRED FLINSTONE';
+
+-- BODMAS
+
+/*
+BRACKETS
+ORDERS
+DIVISION
+MULTIPLICATION
+ADDITION
+SUBTRACTION
+*/
+
+ /*
+operators , + > 
+BRACKETS
+AND
+OR
+ */
+
+use querytask1;
+
+SELECT '104153018' as studentid, M.TITLE, M.RELYEAR
+FROM MOVIE AS M;
+
+select '104153018' as studentid
+
+-- To see a list of the tables in a particular database run the following query
+
+SELECT TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES 
+WHERE TABLE_TYPE='BASE TABLE';
+
+-- show tables is mysql
