8f825574375bd950fc8181e446fec622ba3de018
kayhoweaws <kayhowe@amazon.com>
2022-10-25 11:07:19 +1100

    Create 20221025_Practice_AT1_DBDesignSpec.sql

---
+++ Database/20221025_Practice_AT1_DBDesignSpec.sql
@@ -0,0 +1,15 @@
+-- shift+ctrl+e = execute
+select * 
+from sys.databases
+
+-- use kayhoweDB;
+
+SELECT *
+FROM INFORMATION_SCHEMA.TABLES;
+
+DROP DATABASE kayhoweDB
+;
+
+create database BUILD3018
+;
+
